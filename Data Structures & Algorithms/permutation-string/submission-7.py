6aafeb52600fa72600b5053e501c9fcb17e00993
Tachybanyanya <c-hi.yang@hotmail.sg>
2026-08-29 19:23:31 +0800

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    # Brute Force
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_counted = defaultdict(int)
+        for s in s1:
+            s1_counted[s] += 1
+        s1_n = len(s1)
+        # check if the current str is in the s1 list
+        left = 0
+        s2_counted = defaultdict(int)
+        for i in range(len(s2)):
+            if s2[i] not in s1_counted:
+                continue
+            window = s2[i:i+s1_n]
+            s2_counted = defaultdict(int)
+            for s in window:
+                s2_counted[s] += 1
+            if s1_counted == s2_counted:
+                return True
+        return False
