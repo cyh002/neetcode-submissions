51b592cb454ad77ae25921209540402ca3cc976f
Tachybanyanya <c-hi.yang@hotmail.sg>
2026-09-01 19:36:43 +0800

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Montonic Decreasing Stack
+        results = [0] * len(temperatures)
+        pending = []
+        n_temp = len(temperatures)
+        for i in range(n_temp):
+            while pending and temperatures[pending[-1]] < temperatures[i]:
+                prev_idx = pending[-1]
+                diff_idx = i - prev_idx
+                results[prev_idx] = diff_idx
+                pending.pop()
+            pending.append(i)
+        return results
+        
